6639272e2dc9655cb9d7469a77ad60d338579885
MatMath <mathieu.k.legault@gmail.com>
2016-05-10 11:55:20 -0400

    Layout list of what components I aim for.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,22 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      	TODO: Add Bootstrap classes.
+  		50% width Container for the Building components inside should be a list of different Element of the structure
+  		<div>Select Building Type -> Dropdown from a fetched list</div>
+  		<div>Select Building Neiborhood -> Autocomplete from a fetched list</div>
+  		<div>Building Value + Down Payment OR value -> Extract value out of the Building down back into the Calculation</div>
+  		<div>Select Mortgage % value</div>
+  		<div>Estimate The average rent</div>
+  		<div>Fix expenses % taxes</div>
+  		<div>Welcome taxes Calculation display</div>
+  		<div>Total to pay</div>
+  		<div>Collapsable table of the expenses by months? year?</div>
+  		<div>d3 Graph of the result expected</div>
       </div>
     );
   }
