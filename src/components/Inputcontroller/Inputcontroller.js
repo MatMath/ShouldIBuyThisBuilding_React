7f9262f388adf33ea19d996ba8b6d63c4a41a6dd
MatMath <mathieu.k.legault@gmail.com>
2016-05-13 15:28:08 -0400

    Adding Down payment, Bootstrap and some CSS. Improved the layout to make it more visual.

--- src/components/Inputcontroller/Inputcontroller.js
+++ src/components/Inputcontroller/Inputcontroller.js
@@ -15,64 +15,122 @@ function renderSuggestion(suggestion) {
     <span>{suggestion.City}, {suggestion.Code}</span>
   );
 }
+function convertToCurrency(valueToConvert) {
+    // http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+    let returnNumber = parseFloat(valueToConvert);
+    if (returnNumber) {
+      return returnNumber.toFixed(0).replace(/./g, function(c, i, a) {
+        return i && c !== '.' && ((a.length - i) % 3 === 0) ? ',' + c : c;
+      });
+    }
+}
 
 class Inputcontroller extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       loading:false,
-      mortgageValue: 200000,
-      intRate: ''
+      houseValue: 200000,
+      downPayment: 10,
+      intRate: '',
+      fixExpenses: 1.75
     }
   }
-  changeMortgageValue (event) {
+  changeHouseValue (event) {
     // This set the Mortgage value
-    this.setState({mortgageValue: event.target.value});
+    this.setState({houseValue: event.target.value});
   }
   changeIntRate (event) {
     // This Set the Interest Rate
     this.setState({intRate: event.target.value});
   }
+  changeFixExpenses (event) {
+    // This Set the Interest Rate
+    this.setState({fixExpenses: event.target.value});
+  }
+  changedownPayment (event) {
+    // This Set the Interest Rate
+    this.setState({downPayment: event.target.value});
+  }
   render() {
     const {buildingTypeList, neiborhoodlist, interestRate} = this.props;
-    const {mortgageValue} = this.state;
+    const {houseValue, downPayment, fixExpenses} = this.state;
     return (
-      <div>
-        <hr />
-        <div>
-          Select Building Type:
-          <select>
-            {buildingTypeList.map((building, i) => {
-                const { Title, Appartment} = building
-                return (<option key={i} value={Appartment}>{Title}</option>)
-            })}
-          </select>
-          {/*
-            This is to find the number of appartment in the building
-            TODO: OnClick add the value somewhere???
-          */}
+      <div className='container striped'>
+        <div className='row'>
+          <div className='col-sm-2'>Building Type: </div>
+          <div className='col-sm-6'>
+            <select>
+              {buildingTypeList.map((building, i) => {
+                  const { Title, Appartment} = building
+                  return (<option key={i} value={Appartment}>{Title}</option>)
+              })}
+            </select>
+          </div>
+          <div className='col-sm-4'>Selection of the building</div>
         </div>
-        <SelectNeiborhood neiborhoodlist={neiborhoodlist} />
-        <div>
-          Mortgage value:
+
+        <div className='row'>
+        <div className='col-sm-2'>Neiborhood:</div>
+        <div className='col-sm-6'><SelectNeiborhood neiborhoodlist={neiborhoodlist} /></div>
+        <div className='col-sm-4'>Result is: How to extract it???</div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>House Value: </div>
+        <div className='col-sm-6'>
           <input
             type='number'
-            defaultValue={mortgageValue}
-            onChange={this.changeMortgageValue.bind(this)}/>
-            {this.state.mortgageValue}
+            defaultValue={houseValue}
+            onChange={this.changeHouseValue.bind(this)}/>
         </div>
-        <div>Selec Mortgage %
+        <div className='col-sm-4'>{convertToCurrency(houseValue)} $ </div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>Down Payment %: </div>
+        <div className='col-sm-6'>
+          <input
+            type='number'
+            defaultValue={downPayment}
+            onChange={this.changedownPayment.bind(this)}/>
+        </div>
+        <div className='col-sm-4'>{convertToCurrency(houseValue*downPayment/100)} $ </div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>Interest Rate %: </div>
+        <div className='col-sm-6'>
           <select onChange={this.changeIntRate.bind(this)}>
             {interestRate.map((intRate, i) => {
                 const { name, latestIntRate} = intRate
                 return (<option key={i} value={latestIntRate}>{name} - {latestIntRate}</option>)
             })}
           </select>
-          {this.state.intRate}
         </div>
-        <div><button>Estimate Rent per block</button> rent: (Value Here)</div>
-        <div>Fix expenses % taxes</div>
-        <div>Welcome taxes Calculation display</div>
+        <div className='col-sm-4'>{this.state.intRate}</div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'><button className='btn btn-info'>Estimate Rent per block</button></div>
+        <div className='col-sm-6'>AutoSelectedValue Here</div>
+        <div className='col-sm-4'>Final Value here</div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>Fix expenses % (taxes):</div>
+        <div className='col-sm-6'>
+          <input
+            type='number'
+            defaultValue={fixExpenses}
+            onChange={this.changeFixExpenses.bind(this)}/></div>
+        <div className='col-sm-4'>{fixExpenses} % or {convertToCurrency(houseValue*fixExpenses/100)}$/year</div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>Welcome taxes Calculation display</div>
+        <div className='col-sm-10'></div>
+        </div>
       </div>
    );
   }
@@ -117,7 +175,7 @@ class SelectNeiborhood extends React.Component {
       onChange: this.onChange
     };
     return (
-      <div>Selec Building Neiborhood
+      <div>
         <Autosuggest suggestions={suggestions}
              onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
              getSuggestionValue={getSuggestionValue}
