fde89744dfdaec8634d204d47ed7aa2400497e1c
MatMath <mathieu.k.legault@gmail.com>
2016-05-18 14:06:06 -0400

    Making Autosuggest display on top of the Bootstrap element. Lot of Bootstrap form improvement

--- src/components/Inputcontroller/Inputcontroller.js
+++ src/components/Inputcontroller/Inputcontroller.js
@@ -65,12 +65,12 @@ class Inputcontroller extends React.Component {
   }
   render() {
     const {buildingTypeList, neiborhoodlist, interestRate} = this.props;
-    const {houseValue, downPayment, fixExpenses, intRate, nbrYears, nbrAppartment, oneTimeExpenses, isDisabled} = this.state;
+    const {houseValue, downPayment, fixExpenses, intRate, nbrYears, averageRent, nbrAppartment, oneTimeExpenses, isDisabled} = this.state;
     return (
       <div>
         <div className='row'>
           <div className='col-sm-2'>Building Type: </div>
-          <div className='col-sm-6'>
+          <div className='col-sm-3'>
             <select onChange={this.changeNumberValue.bind(this, 'nbrAppartment')}>
               {buildingTypeList.map((building, i) => {
                   const { Title, Appartment} = building
@@ -78,7 +78,7 @@ class Inputcontroller extends React.Component {
               })}
             </select>
           </div>
-          <div className='col-sm-4'>
+          <div className='col-sm-3'>
             <div className="form-group">
                 <div className="input-group">
                   <div className="input-group-addon"># Rented</div>
@@ -88,41 +88,60 @@ class Inputcontroller extends React.Component {
                 </div>
             </div>
           </div>
+          <div className='col-sm-4'> </div>
         </div>
 
         <div className='row'>
         <div className='col-sm-2'>Neiborhood:</div>
-        <div className='col-sm-6'><SelectNeiborhood neiborhoodlist={neiborhoodlist} neiborhoodSelected={this.neiborhoodSelected}/></div>
+        <div className='col-sm-6'>
+          <SelectNeiborhood className='form-control'neiborhoodlist={neiborhoodlist} neiborhoodSelected={this.neiborhoodSelected}/></div>
         <div className='col-sm-4'>{this.state.neiborhoodName} with #{this.state.neiborhoodCode}</div>
         </div>
 
         <div className='row'>
         <div className='col-sm-2'>House Value: </div>
         <div className='col-sm-6'>
-          <input
-            key='houseValue'
-            type='number'
-            defaultValue={houseValue}
-            onChange={this.changeNumberValue.bind(this, 'houseValue')}/>
+          <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon">$</div>
+                <input type="number" className="form-control"
+                  value={houseValue}
+                  onChange={this.changeNumberValue.bind(this, 'houseValue')} />
+              </div>
+          </div>
         </div>
         <div className='col-sm-4'>{convertToCurrency(houseValue)} $ </div>
         </div>
 
         <div className='row'>
-        <div className='col-sm-2'>Down Payment %: </div>
-        <div className='col-sm-6'>
-          <input
-            key='downPayment'
-            type='number'
-            defaultValue={downPayment}
-            onChange={this.changeNumberValue.bind(this, 'downPayment')}/>
+        <div className='col-sm-2'>Down Payment: </div>
+        <div className='col-sm-3'>
+          <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon">%</div>
+                <input type="number" className="form-control"
+                  value={downPayment}
+                  onChange={this.changeNumberValue.bind(this, 'downPayment')} />
+              </div>
+          </div>
         </div>
-        <div className='col-sm-4'>{convertToCurrency(houseValue*downPayment/100)} $ </div>
+        <div className='col-sm-3 text-center'>{convertToCurrency(houseValue*downPayment/100)} $ </div>
+        <div className='col-sm-4'>input a sidebar control</div>
         </div>
 
         <div className='row'>
-        <div className='col-sm-2'>Interest Rate %: </div>
-        <div className='col-sm-6'>
+        <div className='col-sm-2'>Interest Rate: </div>
+        <div className='col-sm-2'>
+          <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon">%</div>
+                <input type="number" className="form-control"
+                  value={intRate}
+                  onChange={this.changeNumberValue.bind(this, 'intRate')} />
+              </div>
+          </div>
+        </div>
+        <div className='col-sm-4'>
           <select onChange={this.changeNumberValue.bind(this, 'intRate')}>
             {interestRate.map((intRate, i) => {
                 const { name, latestIntRate} = intRate
@@ -130,51 +149,69 @@ class Inputcontroller extends React.Component {
             })}
           </select>
         </div>
-        <div className='col-sm-4'><input
-            key='intRate'
-            type='number'
-            value={intRate}
-            onChange={this.changeNumberValue.bind(this, 'intRate')}/>
-        </div>
+        <div className='col-sm-4'>input a sidebar control</div>
         </div>
 
         <div className='row'>
-        <div className='col-sm-2'>Nbr of Years</div>
+        <div className='col-sm-2'>Period</div>
         <div className='col-sm-6'>
-          <input
-            key='nbrYears'
-            type='number'
-            defaultValue={nbrYears}
-            onChange={this.changeNumberValue.bind(this, 'nbrYears')}/></div>
-        <div className='col-sm-4'>{nbrYears}</div>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">Years:</div>
+              <input type="number" className="form-control"
+                value={nbrYears}
+                onChange={this.changeNumberValue.bind(this, 'nbrYears')} />
+            </div>
+          </div>
+        </div>
+        <div className='col-sm-4'>input a sidebar control</div>
         </div>
 
         <div className='row'>
-        <div className='col-sm-2'><button className='btn btn-info'>Estimate Rent per block</button></div>
-        <div className='col-sm-6'>AutoSelectedValue Here</div>
-        <div className='col-sm-4'>Final Value here</div>
-        </div>
-
-        <div className='row'>
-        <div className='col-sm-2'>Fix expenses % (taxes, repair, fee):</div>
+        <div className='col-sm-2'><button className='btn btn-info'>Estimate Rent</button></div>
         <div className='col-sm-6'>
-          <input
-            key='fixExpenses'
-            type='number'
-            defaultValue={fixExpenses}
-            onChange={this.changeNumberValue.bind(this, 'fixExpenses')}/></div>
-        <div className='col-sm-4'>{parseFloat(fixExpenses).toFixed(2)} % or {convertToCurrency(houseValue*fixExpenses/100)}$/year</div>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">$</div>
+              <input type="number" className="form-control"
+                value={averageRent}
+                onChange={this.changeNumberValue.bind(this, 'averageRent')} />
+            </div>
+          </div>
+        </div>
+        <div className='col-sm-4'></div>
+        </div>
+
+        <div className='row'>
+        <div className='col-sm-2'>Fix expenses (taxes, repair, fee):</div>
+        <div className='col-sm-3'>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">%</div>
+              <input type="number" className="form-control"
+                value={fixExpenses}
+                onChange={this.changeNumberValue.bind(this, 'fixExpenses')} />
+            </div>
+          </div>
+        </div>
+        <div className='col-sm-3'>{parseFloat(fixExpenses).toFixed(2)} % or {convertToCurrency(houseValue*fixExpenses/100)}$/year</div>
+        <div className='col-sm-4'>input a sidebar control</div>
         </div>
 
         <div className='row'>
         <div className='col-sm-2'>Welcome taxes & moving in</div>
-        <div className='col-sm-6'>
-          <input
-            key='oneTimeExpenses'
-            type='number'
-            defaultValue={oneTimeExpenses}
-            onChange={this.changeNumberValue.bind(this, 'oneTimeExpenses')}/></div>
-        <div className='col-sm-4'>{parseFloat(oneTimeExpenses).toFixed(2)} % or {convertToCurrency(houseValue*oneTimeExpenses/100)}$</div>
+        <div className='col-sm-3'>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">%</div>
+              <input type="number" className="form-control"
+                value={oneTimeExpenses}
+                onChange={this.changeNumberValue.bind(this, 'oneTimeExpenses')} />
+            </div>
+          </div>
+        </div>
+        <div className='col-sm-3'>{parseFloat(oneTimeExpenses).toFixed(2)} % or {convertToCurrency(houseValue*oneTimeExpenses/100)}$</div>
+        <div className='col-sm-4'>input a sidebar control</div>
         </div>
 
         <div className='row'>
